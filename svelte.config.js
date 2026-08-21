9181f7b18301d3ab553b272e9f8b5a42d617965f
Tanimal <Bobcheng.bob@gmail.com>
2026-08-21 22:13:41 +0800

    fix assests missing

--- svelte.config.js
+++ svelte.config.js
@@ -15,9 +15,6 @@ const config = {
       precompress: false,
       strict: true,
     }),
-    paths: {
-      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
-    },
   },
   preprocess: [mdsvex({ extensions: mdExtensions })],
   extensions: [".svelte", ...mdExtensions],
